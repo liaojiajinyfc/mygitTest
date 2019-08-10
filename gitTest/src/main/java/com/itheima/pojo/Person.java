d68c5f04523b8e2791743f00f2247a38ac5e6aa6
liaojiajin <1179463723@qq.com>
2019-08-10 16:07:27 +0800

    第四次修改

--- gitTest/src/main/java/com/itheima/pojo/Person.java
+++ gitTest/src/main/java/com/itheima/pojo/Person.java
@@ -3,4 +3,13 @@ package com.itheima.pojo;
 public class Person {
     private int a;
     private int b;
+    private int c;
+    private int d;
+
+    public Person(int a, int b, int c, int d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
 }
