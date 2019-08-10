840450422f27047d937bf76e305a73f81ea8c11f
liaojiajin <1179463723@qq.com>
2019-08-10 16:40:16 +0800

    第五次修改

--- gitTest/src/main/java/com/itheima/pojo/Person.java
+++ gitTest/src/main/java/com/itheima/pojo/Person.java
@@ -12,4 +12,36 @@ public class Person {
         this.c = c;
         this.d = d;
     }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
 }
